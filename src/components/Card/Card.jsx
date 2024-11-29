2a5fae5885d50f158edb90424b313befacfb1765
MattiaPlacido <placido.mattia@gmail.com>
2024-11-29 15:57:52 +0100

    Progressi

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,37 @@
 import styles from "./card.module.css";
 
-export default function Card() {
+function getTagColor(tag) {
+  switch (tag.toLowerCase()) {
+    case "css":
+      return styles.bg_red;
+    case "js":
+      return styles.bg_brown;
+    case "html":
+      return styles.bg_blue;
+    case "php":
+      return styles.bg_violet;
+    default:
+      return ""; // Classe predefinita o nessuna classe
+  }
+}
+export default function Card({ id, title, image, content, tags, published }) {
   return (
-    <div className={styles.card}>
-      <img src="https://placehold.co/600x400" />
+    <div className={styles.card} id={id}>
+      <img src={image || "https://placehold.co/600x400"} />
       <div className={styles.container}>
         <h4>
-          <b>Titolo del post</b>
+          <b>{title}</b>
         </h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-          doloremque culpa facilis quo, sequi quis nostrum voluptatem dolore
-          corporis at tenetur corrupti ab, exercitationem animi fugit numquam
-          consectetur perspiciatis impedit.
-        </p>
-        <button className={styles.button}>LEGGI DI PIU</button>
+        <p>{content}</p>
+        <div class={styles.container}>
+          <button className={styles.button}>LEGGI DI PIU</button>
+          {tags.length > 0 &&
+            tags.map((tag) => (
+              <span className={`${styles.badge} ${getTagColor(tag)}`}>
+                {tag}
+              </span>
+            ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,23 @@
 import styles from "./main.module.css";
 import Card from "../Card/Card.jsx";
-
+import { posts } from "../data/posts.jsx";
 
 export default function Main() {
   return (
     <main className={styles.main}>
       <h2>Il mio blog</h2>
-      <Card />
+      <div class={styles.container}>
+        {posts.map((post) => (
+          <Card
+            id={post.id}
+            title={post.title}
+            image={post.image}
+            content={post.content}
+            tags={post.tags}
+            published={post.published}
+          />
+        ))}
+      </div>
     </main>
   );
 }
